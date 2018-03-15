99ee34cebc7245ff8cc0ced9029299cc070d6028
Luis <cunhaluisg5@gmail.com>
2018-03-15 10:52:05 -0300

    Removida opção de redimensionar tela

--- Livraria/src/forms/FormPrincipal.java
+++ Livraria/src/forms/FormPrincipal.java
@@ -41,6 +41,7 @@ public class FormPrincipal extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Sistema de Controle da Livraria Livro Aberto");
+        setResizable(false);
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowOpened(java.awt.event.WindowEvent evt) {
                 formWindowOpened(evt);
