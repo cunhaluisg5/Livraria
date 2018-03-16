1be3758a39665cf9afcf479228cb1360b5e15b11
cunhaluisg5 <cunhaluisg5@gmail.com>
2018-03-16 11:51:07 -0300

    Criada a classe Usuario

---
+++ Livraria/src/model/Usuario.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Luís Gustavo
+ */
+public class Usuario {
+    private String nome;
+    private String matricula;
+    private String senha;
+
+    public Usuario() {
+        this.nome = "Admin";
+        this.matricula = "AC2018";
+        this.senha = "s1234";
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+}
